c81aa844860ae722376e36578b678b6b145bc5ff
Wyatt Burgess <wyattpburgess@gmail.com>
2022-04-21 20:50:48 -0400

    Setup priority queue with max heap

--- practice/linked-lists/priorityQueue.js
+++ practice/linked-lists/priorityQueue.js
@@ -8,54 +8,110 @@ class Node {
 
 class PriorityQueue {
     constructor() {
-        this.head = null;
-        this.length = null;
+        this.size = 0;
+        this.items = [];
+    }
+
+    getParentIndex(idx) {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    getLeftChildIndex(idx) {
+        return idx * 2 + 1;
+    }
+
+    getRightChildIndex(idx) {
+        return idx * 2 + 2;
+    }
+
+    hasLeftChild(idx) {
+        return this.items[this.getLeftChildIndex(idx)] !== undefined;
+    }
+
+    hasRightChild(idx) {
+        return this.items[this.getRightChildIndex(idx)] !== undefined;
+    }
+
+    hasParent(idx) {
+        return this.items[this.getParentIndex(idx)] !== undefined
+    }
+
+    leftChild(idx) {
+        return this.items[this.getLeftChildIndex(idx)];
+    }
+
+    rightChild(idx) {
+        return this.items[this.getRightChildIndex(idx)];
+    }
+
+    parent(idx) {
+        return this.items[this.getParentIndex(idx)];
+    }
+
+    peek() {
+        if (this.items.length === 0) return null;
+        return this.items[0];
     }
 
     enqueue(weight, value) {
         const newNode = new Node(weight, value);
-        if (!this.head) {
-            this.head = newNode;
-
-            this.length++;
-            return this;
-        }
-        let prevNode = null;
-        let nextNode = this.head;
-        while (nextNode !== null && newNode.weight <= nextNode.weight) {
-            prevNode = nextNode;
-            nextNode = nextNode.next;
-        }
-        if (prevNode !== null) {
-            prevNode.next = newNode;
-        } else {
-            this.head = newNode;
-        }
-        newNode.next = nextNode;
-        this.length++;
-        return this;
+        this.items.push(newNode);
+        this.bubbleUp();
+        return this.items;
     }
 
     dequeue() {
-        const temp = this.head;
-        if (this.head) {
-            this.head = this.head.next;
-            this.length--;
-        }
-        return temp;
+        if (this.items.length === 0) return null;
+        const topItem = this.items[0];
+        const lastItem = this.items[this.items.length - 1];
+        // remove item from end
+        this.items.pop();
+        // add item to top and bubble down
+        this.items[0] = lastItem;
+        this.bubbleDown();
+        return topItem;
     }
 
-    peek() {
-        return this.head;
+    swap(idxOne, idxTwo) {
+        const temp = this.items[idxOne];
+        this.items[idxOne] = this.items[idxTwo];
+        this.items[idxTwo] = temp;
     }
+
+    bubbleUp() {
+        let idx = this.items.length - 1;
+        while (this.hasParent(idx) && this.parent(idx).weight < this.items[idx].weight) {
+            this.swap(this.getParentIndex(idx), idx);
+            idx = this.getParentIndex(idx);
+        }
+    }
+
+    bubbleDown() {
+        let idx = 0;
+        while (this.hasLeftChild(idx)) {
+            let largerChildIdx = this.getLeftChildIndex(idx);
+            let largerChildWeight = this.items[largerChildIdx].weight;
+            if (this.hasRightChild(idx) && this.rightChild(idx).weight > largerChildWeight) {
+                largerChildIdx = this.getRightChildIndex(idx);
+            }
+            if (this.items[idx].weight > this.items[largerChildIdx].weight) break;
+            this.swap(idx, largerChildIdx);
+            idx = largerChildIdx;
+        }
+    }
+
+
 }
 
 const pQ = new PriorityQueue();
 pQ.enqueue(10, "Lebron");
-pQ.enqueue(2, "Kobe");
+pQ.enqueue(12, "Kobe");
+pQ.enqueue(100, "Scalabrine");
+pQ.enqueue(50, "Chalmers");
 pQ.enqueue(20, "Jordan");
-pQ.enqueue(1, "Scalabrine");
-pQ.dequeue();
-pQ.dequeue();
 console.log(pQ);
-
+console.log(pQ.dequeue());
+console.log(pQ.dequeue());
+console.log(pQ.dequeue());
+console.log(pQ.dequeue());
+console.log(pQ.dequeue());
